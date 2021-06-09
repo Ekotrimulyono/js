4e2149034f3e3472b2adc4ebcdcff4e5366bdb08
Ekotrimulyono <55267211+Ekotrimulyono@users.noreply.github.com>
2021-06-09 10:03:00 +0700

    Bio Fb Keren Generator
    Ini adalah hasil dari belajar js sendiri yang masih belum bisa saya buat seperti yang saya inginkan.
    
    Disini hasil dari setiap font akan terpisah, padahal saya pengen buatnya itu dari font a dan b langsung nyambung. Tapi dikarenakan saya kurang paham mengatur line break sehingga jadinya masih pisah.

---
+++ BioFbKeren.js
@@ -0,0 +1,124 @@
+var biofbkeren = {
+  q: "\n╱◢▔◣\n╱▉╱▉\n╱◥▁▉\n╱╱╱◥◣\n",
+  w: "\n╱◥◣╱╱◢◣╱╱◢◤\n╱╱◥◣◢◤◥◣◢◤\n╱╱╱◥◤╱╱◥◤╱\n╱╱╱╱╱╱╱╱╱╱\n",
+  e: "\n╱▉▔◤\n╱▉■■\n╱▉▂◤\n╱╱╱╱\n",
+  r: "\n╱▉▔◣\n╱▉▁◤\n╱▉◥◣\n╱╱╱╱\n",
+  t: "\n◢▔▉▔◣\n╱╱▉╱╱\n╱╱▉╱╱\n╱╱╱╱╱\n",
+  y: "\n╱▉╱▉\n╱◥▁▉\n╱◥▁◤\n╱╱╱╱\n",
+  u: "\n╱▉╱▉\n╱▉╱▉\n╱◥▁◤\n╱╱╱╱\n",
+  i: "\n╱▉\n╱▉\n╱▉\n╱╱\n",
+  o: "\n╱◢▔▔◣\n╱▉╱╱▉\n╱◥▁▁◤\n╱╱╱╱╱\n",
+  p: "\n╱▉▔◣\n╱▉▁■\n╱▉╱╱\n╱╱╱╱\n",
+Q: "\n╱◢▔◣\n╱▉╱▉\n╱◥▁▉\n╱╱╱◥◣\n",
+  W: "\n╱◥◣╱╱◢◣╱╱◢◤\n╱╱◥◣◢◤◥◣◢◤\n╱╱╱◥◤╱╱◥◤╱\n╱╱╱╱╱╱╱╱╱╱\n",
+  E: "\n╱▉▔◤\n╱▉■■\n╱▉▂◤\n╱╱╱╱\n",
+  R: "\n╱▉▔◣\n╱▉▁◤\n╱▉◥◣\n╱╱╱╱\n",
+  T: "\n◢▔▉▔◣\n╱╱▉╱╱\n╱╱▉╱╱\n╱╱╱╱╱\n",
+  Y: "\n╱▉╱▉\n╱◥▁▉\n╱◥▁◤\n╱╱╱╱\n",
+  U: "\n╱▉╱▉\n╱▉╱▉\n╱◥▁◤\n╱╱╱╱\n",
+  I: "\n╱▉\n╱▉\n╱▉\n╱╱\n",
+  O: "\n╱◢▔▔◣\n╱▉╱╱▉\n╱◥▁▁◤\n╱╱╱╱╱\n",
+  P: "\n╱▉▔◣\n╱▉▁■\n╱▉╱╱\n╱╱╱╱\n",
+a: "\n╱◢▔◣\n╱▉▁▉\n╱▉╱▉\n╱╱╱╱\n",
+  s: "\n╱◢▔◤\n╱◥▉◣\n╱◢▂◤\n╱╱╱╱\n",
+  d: "\n╱▉▔◣\n╱▉╱▉ \n╱▉▁◤\n╱╱╱╱\n",
+  f: "\n╱◢▔▔\n╱▉■■\n╱▉╱╱\n╱╱╱╱\n",
+  g: "\n╱▉▔◣\n╱▉▔◤\n╱◥▁◤\n╱╱╱╱\n",
+  h: "\n╱▉╱▉\n╱▉■▉\n╱▉╱▉\n╱╱╱╱\n",
+  j: "\n╱╱╱▉\n╱╱╱▉\n╱◥▁◤\n╱╱╱╱\n",
+  k: "\n╱▉╱◣\n╱▉◢◤\n╱▉◥◣\n╱◥╱╱\n",
+  l: "\n╱▉╱╱\n╱▉╱╱\n╱▉▂◣\n╱╱╱╱\n",
+  A: "\n╱◢▔◣\n╱▉▁▉\n╱▉╱▉\n╱╱╱╱\n",
+  S: "\n╱◢▔◤\n╱◥▉◣\n╱◢▂◤\n╱╱╱╱\n",
+  D: "\n╱▉▔◣\n╱▉╱▉ \n╱▉▁◤\n╱╱╱╱\n",
+  F: "\n╱◢▔▔\n╱▉■■\n╱▉╱╱\n╱╱╱╱\n",
+  G: "\n╱▉▔◣\n╱▉▔◤\n╱◥▁◤\n╱╱╱╱\n",
+  H: "\n╱▉╱▉\n╱▉■▉\n╱▉╱▉\n╱╱╱╱\n",
+  J: "\n╱╱╱▉\n╱╱╱▉\n╱◥▁◤\n╱╱╱╱\n",
+  K: "\n╱▉╱◣\n╱▉◢◤\n╱▉◥◣\n╱◥╱╱\n",
+  L: "\n╱▉╱╱\n╱▉╱╱\n╱▉▂◣\n╱╱╱╱\n",
+z: "\n╱◢▔◣\n╱◢▉◤\n╱◥▁◤\n╱╱╱╱\n",
+  x: "\n╱◥◣╱╱◢◤\n╱╱◥◣◢◤╱\n╱╱◢◤◥◣╱\n╱◢◤╱╱◥◣\n",
+  c: "\n╱◢▔◣\n╱▉╱╱\n╱◥▁◤\n╱╱╱╱\n",
+  v: "\n◥◣╱╱◢◤\n╱◥◣◢◤\n╱╱◥◤╱\n╱╱╱╱╱\n",
+  b: "\n╱▉▔◣\n╱▉■▉\n╱▉▁◤\n╱╱╱╱\n",
+  n: "\n╱▉◣▉\n╱▉◥▉ \n╱▉╱▉\n╱╱╱╱\n",
+  m: "\n╱▉◣◢▉\n╱▉◥◤▉\n╱▉╱╱▉\n╱╱╱╱╱\n",
+  Z: "\n╱◢▔◣\n╱◢▉◤\n╱◥▁◤\n╱╱╱╱\n",
+  X: "\n╱◥◣╱╱◢◤\n╱╱◥◣◢◤╱\n╱╱◢◤◥◣╱\n╱◢◤╱╱◥◣\n",
+  C: "\n╱◢▔◣\n╱▉╱╱\n╱◥▁◤\n╱╱╱╱\n",
+  V: "\n◥◣╱╱◢◤\n╱◥◣◢◤\n╱╱◥◤╱\n╱╱╱╱╱\n",
+  B: "\n╱▉▔◣\n╱▉■▉\n╱▉▁◤\n╱╱╱╱\n",
+  N: "\n╱▉◣▉\n╱▉◥▉ \n╱▉╱▉\n╱╱╱╱\n",
+  M: "\n╱▉◣◢▉\n╱▉◥◤▉\n╱▉╱╱▉\n╱╱╱╱╱\n"
+};
+var bio_input = document.getElementById("bio_input");
+var bio_output = document.getElementById("bio_output");
+
+// Set pointer-events none as default
+bio_output.style.pointerEvents = "none";
+
+bio_input.addEventListener(
+  "input",
+  function() {
+    bio_output.value = "";
+    var val = this.value;
+    val = val.split("");
+if (bio_input.value == "") {
+      bio_output.style.pointerEvents = "none";
+    } else {
+      bio_output.style.pointerEvents = "auto";
+    }
+
+    setTimeout(function() {
+      Object.keys(biofbkeren).map(function(item) {
+        //Put for loop to convert same letters also
+        for (var j = 0; j < val.length; j++) {
+          var i = val.indexOf(item);
+          if (i !== -1) {
+            val[i] = biofbkeren[item];
+            bio_output.value = val.join(" ");
+// Detect line break
+            if (val.indexOf("\n") !== -1) {
+              bio_output.value = trimIt(val.join(" "));
+            }
+          }
+        }
+      });
+    }, 50);
+  },
+  false
+);
+
+// To trim first letter's white space in new line
+function trimIt(x) {
+  return x.replace(/^\s+|\s+$/gm, "");
+}
+
+// Trigger on paste
+bio_input.addEventListener(
+  "paste",
+  function() {
+    bio_input.keyup();
+  },
+  false
+);
+// Copy to clipboard
+const answer = document.getElementById("copyResult");
+const copy   = document.getElementById("copyButton");
+const selection = window.getSelection();
+const range = document.createRange();
+const textToCopy = document.getElementById("textToCopy")
+
+copy.addEventListener('click', function(e) {
+    range.selectNodeContents(textToCopy);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    const successful = document.execCommand('copy');
+    if(successful){
+      answer.innerHTML = 'Copied!';
+    } else {
+      answer.innerHTML = 'Unable to copy!';  
+    }
+    window.getSelection().removeAllRanges()
+});
